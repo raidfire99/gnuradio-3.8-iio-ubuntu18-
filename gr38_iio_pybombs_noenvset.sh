bd6276dfa4a0370bd7cda949d798a870fb54db73
raidfire99 <37177105+raidfire99@users.noreply.github.com>
2019-12-28 23:38:19 +0200

    Update gr38_iio_pybombs_noenvset.sh

--- gr38_iio_pybombs_noenvset.sh
+++ gr38_iio_pybombs_noenvset.sh
@@ -1,7 +1,7 @@
+sudo apt update
 sudo apt install git
-sudo apt install python3
-sudo apt install python3-pip
-sudo pip3 install --upgrade git+https://github.com/gnuradio/pybombs.git
+sudo apt install python3 -y
+sudo apt install python3-pip -y
 sudo pip3 install --upgrade git+https://github.com/gnuradio/pybombs.git
 pybombs recipes add-defaults
 pybombs prefix init ~/gnuradio -R gnuradio-default
