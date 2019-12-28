6ebb383816ce93c180e4bdf45210d49f35f1bdbf
raidfire99 <37177105+raidfire99@users.noreply.github.com>
2019-12-29 01:39:56 +0200

    Update gr38_iio_pybombs_noenvset.sh

--- gr38_iio_pybombs_noenvset.sh
+++ gr38_iio_pybombs_noenvset.sh
@@ -1,3 +1,5 @@
+echo Gnuradio 3.8 with gr-iio build script, this is going to take some time go make a big pot of coffee, 
+echo script is free for all but with credits to RAIDFIRE@INTERCEPT_RF
 sudo apt update
 sudo apt install git
 sudo apt install python3 -y
