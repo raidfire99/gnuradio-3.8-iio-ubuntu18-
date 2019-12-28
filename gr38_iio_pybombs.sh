6b4c26245683f49fd12b2251f739f39ff9f53cba
raidfire99 <37177105+raidfire99@users.noreply.github.com>
2019-12-29 01:50:27 +0200

    Update gr38_iio_pybombs.sh

--- gr38_iio_pybombs.sh
+++ gr38_iio_pybombs.sh
@@ -1,4 +1,5 @@
-echo Gnuradio 3.8 with gr-iio build script, this is going to take some time go make a big pot of coffee, 
+echo Gnuradio 3.8 with gr-iio build script, this is going to take some time,
+echo how about a nice game of chess?
 echo script is free for all but with credits to RAIDFIRE@INTERCEPT_RF
 sudo apt update
 sudo apt install git
