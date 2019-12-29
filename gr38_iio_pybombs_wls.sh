00936fcfd183b724208cfff2dcd8f63efba683b6
raidfire99 <37177105+raidfire99@users.noreply.github.com>
2019-12-29 11:17:11 +0200

    Update gr38_iio_pybombs_wls.sh

--- gr38_iio_pybombs_wls.sh
+++ gr38_iio_pybombs_wls.sh
@@ -2,7 +2,6 @@ echo Gnuradio 3.8 with gr-iio build script, this is going to take some time ,
 echo how about a nice game of chess?
 echo script is free for all but with credits to RAIDFIRE@INTERCEPT_RF
 sudo apt update
-sudo apt install git
 sudo apt install python3 -y
 sudo apt install python3-pip -y --fix-missing
 sudo pip3 install --upgrade git+https://github.com/gnuradio/pybombs.git
