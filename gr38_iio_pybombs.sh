2ae11f7c734babfc522955a46c199de24f1e5a46
raidfire99 <37177105+raidfire99@users.noreply.github.com>
2021-07-05 23:08:48 +0300

    Update gr38_iio_pybombs.sh

--- gr38_iio_pybombs.sh
+++ gr38_iio_pybombs.sh
@@ -9,3 +9,4 @@ pybombs recipes add-defaults
 pybombs prefix init ~/gnuradio -R gnuradio-default
 pybombs install gr-iio
 source ~/gnuradio/setup_env.sh
+echo source ~/gnuradio/setup_env.sh >> .bashrc
